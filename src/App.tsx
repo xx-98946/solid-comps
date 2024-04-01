ba5bc65118282d1620377bf23b1452327c14be32
xuxin <2821550908@qq.com>
2024-04-01 23:18:55 +0800

    add: ctrl+k 使得输入框聚焦

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Router, Route } from "@solidjs/router";
+import { HashRouter, Route } from "@solidjs/router";
 import Home from "./pages/Home";
 import Demo from "./pages/Demo";
 
 export default function App() {
     return (
-        <Router>
+        <HashRouter>
             <Route path="/" component={Home}></Route>
             <Route path="/demo" component={Demo}></Route>
-        </Router>
+        </HashRouter>
     );
 }
